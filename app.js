2670851ea78d06b9c37671893e68adc0c144284c
TaroSunn <sunxiaomingyes@gmail.com>
2021-06-14 16:48:46 +0800

    feat: 接入pug模板

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const server = app.listen(port, () => {
+  console.log('Server listening on port ' + port)
+})
+
+app.set('view engine', 'pug')
+
+app.set('views', 'views')
+
+app.get('/', (req, res, next) => {
+
+  const payload = {
+    pageTitle: 'Home'
+  }
+
+  res.status(200).render('home', payload)
+})
