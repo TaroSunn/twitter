aa70826c5c81f37e244bd216d62e87cd9cc371ef
TaroSunn <sunxiaomingyes@gmail.com>
2021-06-14 20:53:21 +0800

    feat: 登录

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const {requireLogin} = require('./middleware')
+const path = require('path')
+const bodyParser = require('body-parser')
 
 const server = app.listen(port, () => {
   console.log('Server listening on port ' + port)
 })
 
 app.set('view engine', 'pug')
-
 app.set('views', 'views')
 
-app.get('/', (req, res, next) => {
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+// Routes
+const loginRoute = require('./routes/loginRoutes')
+const registerRoute = require('./routes/registerRoutes')
+
+app.use('/login', loginRoute)
+app.use('/register', registerRoute)
+
+
+app.get('/', requireLogin, (req, res, next) => {
 
   const payload = {
     pageTitle: 'Home'
